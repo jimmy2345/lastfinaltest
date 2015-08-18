1855d0659f50c67a13d0038e6620fcfedc211546
viralbbhavsar <viralbbhavsar@gmail.com>
2015-08-18 16:35:38 -0400

    changed html files

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Supply a title</title>
+        <title>Supply</title>
     </head>
     <body>
         <?php
